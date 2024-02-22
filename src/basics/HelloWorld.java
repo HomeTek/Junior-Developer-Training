16518a4adc20274de29b4da9c71be26a029c4160
HomeTek <160857765+HomeTek@users.noreply.github.com>
2024-02-22 13:43:26 +0000

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,7 +3,8 @@ package basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		 System.out.println("Hello world");
-		 System.out.println("I'm ading code to my project");
+		 System.out.println("I'm adding code to my project");
+		 System.out.println("Adding code from Github");
 	}
 
 }
